4fb431021f013366f98df86103180b8f48f386bb
NasarethMekuri <tm_maniac@hotmail.com>
2015-04-13 14:53:19 +0200

    First BLOOD!!!!!

---
+++ src/hotelformen/domain/Booking.java
@@ -0,0 +1,10 @@
+package hotelformen.domain;
+
+import java.util.List;
+
+public class Booking
+{
+    private List<Service> _services;
+    private List<Room> _rooms;
+    private boolean _isActive;
+}
