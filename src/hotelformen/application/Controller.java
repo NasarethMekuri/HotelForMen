900b89dd8b28cbe34345169f5cacda3f891c66cd
cymon343 <cymon343@hotmail.com>
2015-04-17 13:13:08 +0200

    Fixed Hotel switch
    Ikk i ården Mården!

--- src/hotelformen/application/Controller.java
+++ src/hotelformen/application/Controller.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package hotelformen.application;
 
 import hotelformen.domain.*;
@@ -16,7 +16,7 @@ import java.util.List;
 public class Controller implements IController
 {
     private Hotel hotel = new Hotel();
-
+    
     @Override
     public List<Room> getAvailableRooms(Date startDate, Date endDate, String type)
     {
@@ -30,32 +30,32 @@ public class Controller implements IController
         Customer customer = getCustomerFromId(customerID);
         return hotel.bookRooms(rooms, startDate, endDate, customer, services);
     }
-
+    
     @Override
     public boolean addServicesToBooking(List<Service> services, int customerID)
     {
         Customer customer = getCustomerFromId(customerID);
         return hotel.addServicesToBooking(services, customer);
     }
-
+    
     @Override
     public Service createService(int id, String name, String description, double price)
     {
         return hotel.createService(id, name, description, price);
     }
-
+    
     @Override
     public void updateService(int id, String name, String description, double price)
     {
         hotel.updateService(id, name, description, price);
     }
-
+    
     @Override
     public void deleteService(int id)
     {
         hotel.deleteService(id);
     }
-
+    
     
     @Override
     public List<Service> getServices()
@@ -65,26 +65,29 @@ public class Controller implements IController
     
     private RoomType getTypeFromString(String _type)
     {
-            switch (_type.toUpperCase())
+        switch (_type.toUpperCase())
         {
+            case "":
+                return null;
+                
             case "SINGLE":
                 return RoomType.SINGLE;
                 
             case "DOUBLE":
                 return RoomType.DOUBLE;
-            
+                
             case "QUEEN":
                 return RoomType.QUEEN;
-            
+                
             case "KING":
                 return RoomType.KING;
-            
+                
             case "SHEIK":
                 return RoomType.SHEIK;
-            
+                
             case "FOR_MEN":
                 return RoomType.FOR_MEN;
-                           
+                
             default:
                 throw new AssertionError("Wrong type"); //TODO-> Handle this in UI
         }
