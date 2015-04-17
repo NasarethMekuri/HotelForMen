78480842f8ebcdf703b88df0fb194bfd48e2cb6d
NasarethMekuri <tm_maniac@hotmail.com>
2015-04-17 14:50:00 +0200

    SVIN MIG!
    Altså Simon

--- src/hotelformen/domain/Hotel.java
+++ src/hotelformen/domain/Hotel.java
@@ -191,7 +191,7 @@ public class Hotel
     {
         //TODO: Handle ID! (ID should be unique, so It cannot be given as param) 
         //Create Service in Database - and fetch the unique ID from the Service table.
-        return _databaseHandler.createService(String name, String description, double price);
+        return _databaseHandler.createService(name, description, price);
     }
     
     public boolean updateService(int id, String name, String description, double price)//TODO: return type must be boolean! change should be verified 
@@ -203,9 +203,10 @@ public class Hotel
                 _services.get(i).setName(name);
                 _services.get(i).setDescripion(description);
                 _services.get(i).setPrice(price);
-                return _services.get(i);
+                return true;
             }
         }
+        return false;
     }
     
     public void updateServiceName(int id, String name)//TODO: return type must be boolean! change should be verified 
